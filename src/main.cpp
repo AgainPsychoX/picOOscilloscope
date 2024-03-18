e7ea9723279aa2bfcb80b95a3bf1b5149c018786
Patryk (PsychoX) Ludwikowski <patryk.ludwikowski.7@gmail.com>
2024-03-18 22:38:06 +0100

    Playing around with (custom) touch calibration

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,292 @@
-// Adapted example 'Touch_calibrate.ino' from TFT_eSPI library.
-
+#include <limits>
 #include <SPI.h>
 #include <TFT_eSPI.h>
+#undef abs
 
 TFT_eSPI tft = TFT_eSPI();
 
-// Code to run a screen calibration, not needed when calibration values set in setup()
-void touch_calibrate()
-{
-	uint16_t calData[5];
+namespace touch {
 
-	// Calibrate
-	tft.fillScreen(TFT_BLACK);
-	tft.setCursor(20, 0);
-	tft.setTextFont(2);
-	tft.setTextSize(1);
-	tft.setTextColor(TFT_WHITE, TFT_BLACK);
+// TODO: Adjustable Z threshold? (detect idle state in the calibration)
+constexpr uint16_t zThreshold = 150;
 
-	tft.println("Touch corners as indicated");
+struct CalibrationData {
+	uint16_t screenWidth;
+	uint16_t screenHeight;
+	int16_t xScale;
+	int16_t yScale;
+	int16_t xOffset;
+	int16_t yOffset;
+	// uint16_t zThreshold;
+	bool rotated; // true if screen X axis is touch Y (and vice-versa)
 
-	tft.setTextFont(1);
-	tft.println();
-
-	tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
-
-	Serial.println(); Serial.println();
-	Serial.println("// Use this calibration code in setup():");
-	Serial.print("  uint16_t calData[5] = ");
-	Serial.print("{ ");
-
-	for (uint8_t i = 0; i < 5; i++)
+	void convertRaw(uint16_t& x, uint16_t& y)
 	{
-		Serial.print(calData[i]);
-		if (i < 4) Serial.print(", ");
+		if (rotated) {
+			uint16_t t = y;
+			y = (x - xOffset) * screenHeight / xScale;
+			x = (t - yOffset) * screenWidth  / yScale;
+		}
+		else {
+			x = (x - xOffset) * screenWidth  / xScale;
+			y = (y - yOffset) * screenHeight / yScale;
+		}
 	}
 
-	Serial.println(" };");
-	Serial.print("  tft.setTouch(calData);");
-	Serial.println(); Serial.println();
+	int snprintf(char* buffer, size_t maxLength) {
+		return std::snprintf(
+			buffer, maxLength, "w=%u h=%u xS=%d yS=%d xO=%d yO=%d",
+			screenWidth, screenHeight, xScale, yScale, xOffset, yOffset
+		);
+	}
+};
+CalibrationData calibrationData;
 
-	tft.fillScreen(TFT_BLACK);
-	
-	tft.setTextColor(TFT_GREEN, TFT_BLACK);
-	tft.println("Calibration complete!");
-	tft.println("Calibration code sent to Serial port.");
-
-	delay(4000);
+bool getTouch(uint16_t& x, uint16_t& y)
+{
+	uint16_t z = tft.getTouchRawZ();
+	if (z >= zThreshold) {
+		tft.getTouchRaw(&x, &y);
+		calibrationData.convertRaw(x, y);
+		return true;
+	}
+	return false;
 }
 
-void setup() {
-	// Use serial port
+bool getTouchAverage(uint16_t& x, uint16_t& y, uint8_t samplesTwoExponent = 4)
+{
+	static uint16_t ax, ay, az;
+	static unsigned long lastTime;
+	uint16_t rx, ry, rz;
+	tft.getTouchRaw(&rx, &ry);
+	rz = tft.getTouchRawZ();
+
+	// TODO: make it distance based instead time 
+	unsigned long now = micros();
+	if (now - lastTime < 1'000) {
+		rx = (ax + rx) / 2;
+		ry = (ay + ry) / 2;
+		rz = (az + rz) / 2;
+	}
+	ax = rx;
+	ay = ry;
+	az = rz;
+	lastTime = now;
+
+	if (rz >= zThreshold) {
+		calibrationData.convertRaw(rx, ry);
+		x = rx;
+		y = ry;
+		return true;
+	}
+	return false;
+}
+
+void calibrate()
+{
+	uint16_t w = tft.width();   // Width of the display
+	uint16_t h = tft.height();  // Height of the display
+	uint8_t as = 15;            // Arrow size (0 means 1px)
+	uint16_t bgc = TFT_BLACK;   // Background color
+	uint16_t ac = TFT_MAGENTA;  // Arrows color
+
+	struct Corner {
+		uint16_t minX = std::numeric_limits<uint16_t>::max();
+		uint16_t minY = std::numeric_limits<uint16_t>::max();
+		uint16_t maxX = 0;
+		uint16_t maxY = 0;
+	};
+	Corner corners[4];
+
+	// 5 steps (corners + clear)
+	for (uint8_t i = 0; i < 5; i++) {
+		// Clear all arrows
+		tft.fillRect(0, 0, as+1, as+1, bgc);
+		tft.fillRect(0, h-as-1, as+1, as+1, bgc);
+		tft.fillRect(w-as-1, 0, as+1, as+1, bgc);
+		tft.fillRect(w-as-1, h-as-1, as+1, as+1, bgc);
+
+		if (i >= 4) break; // only clear
+
+		// Draw arrows
+		switch (i) {
+			case 0: // up left
+				tft.drawLine(0, 0, 0, as, ac);
+				tft.drawLine(0, 0, as, 0, ac);
+				tft.drawLine(0, 0, as, as, ac);
+				break;
+			case 1: // bottom left
+				tft.drawLine(0, h-as-1, 0, h-1, ac);
+				tft.drawLine(0, h-1, as, h-1, ac);
+				tft.drawLine(as, h-as-1, 0, h-1 , ac);
+				break;
+			case 2: // up right
+				tft.drawLine(w-as-1, 0, w-1, 0, ac);
+				tft.drawLine(w-as-1, as, w-1, 0, ac);
+				tft.drawLine(w-1, as, w-1, 0, ac);
+				break;
+			case 3: // bottom right
+				tft.drawLine(w-as-1, h-as-1, w-1, h-1, ac);
+				tft.drawLine(w-1, h-1-as, w-1, h-1, ac);
+				tft.drawLine(w-1-as, h-1, w-1, h-1, ac);
+				break;
+		}
+
+		if (i > 0) delay(1000); // allow user to release from previous arrow
+
+		// Read the values until no more updates
+		uint16_t timeout = 5000;
+		bool anything = false;
+		do {
+			bool updates = false;
+
+			uint16_t x, y, z;
+			z = tft.getTouchRawZ();
+			if (z >= zThreshold) {
+				tft.getTouchRaw(&x, &y);
+
+				if (corners[i].minX > x) {
+					corners[i].minX = x;
+					updates = true;
+				}
+				if (corners[i].minY > y) {
+					corners[i].minY = y;
+					updates = true;
+				}
+				if (corners[i].maxX < x) {
+					corners[i].maxX = x;
+					updates = true;
+				}
+				if (corners[i].maxY < y) {
+					corners[i].maxY = y;
+					updates = true;
+				}
+				
+				if (updates) {
+					anything = true;
+					timeout = 2000;
+				}
+			}
+
+			delay(1);
+		}
+		while (--timeout);
+
+		if (!anything) {
+			// Failed calibration, no touch detected after long time
+			if (Serial) Serial.println("Failed to calibrate!");
+			tft.fillScreen(bgc);
+			tft.fillRect(0, 0, as+1, as+1, TFT_RED);
+			delay(1000);
+			return;
+		}
+	}
+
+	// Use collected data to fill calibration data
+	calibrationData.screenWidth = w;
+	calibrationData.screenHeight = h;
+	auto xVerticalDifference = std::abs(corners[0].maxX - corners[1].maxX);
+	auto xHorizontalDifference = std::abs(corners[0].maxX - corners[2].maxX);
+	calibrationData.rotated = xVerticalDifference > xHorizontalDifference;
+	if (calibrationData.rotated) /* x is vertical, so y is horizontal */ {
+		if (corners[0].maxX < corners[1].maxX) /* x grows towards bottom */ {
+			calibrationData.xOffset = corners[0].minX;
+			calibrationData.xScale = corners[0].minX - corners[1].maxX;
+		}
+		else /* x drops towards bottom */ {
+			calibrationData.xOffset = corners[0].maxX;
+			calibrationData.xScale = corners[1].minX - corners[0].maxX;
+		}
+		if (corners[0].maxY < corners[2].maxY) /* y grows towards right */ {
+			calibrationData.yOffset = corners[0].minY;
+			calibrationData.yScale = corners[0].minY - corners[2].maxY;
+		}
+		else /* y drops towards right */ {
+			calibrationData.yOffset = corners[0].maxY;
+			calibrationData.yScale = corners[2].minY - corners[0].maxY;
+		}
+	}
+	else /* x is horizontal, so y is vertical */ {
+		if (corners[0].maxX < corners[2].maxX) /* x grows towards right */ {
+			calibrationData.xOffset = corners[0].minX;
+			calibrationData.xScale = corners[0].minX - corners[2].maxX;
+		}
+		else /* x drops towards right */ {
+			calibrationData.xOffset = corners[0].maxX;
+			calibrationData.xScale = corners[2].minX - corners[0].maxX;
+		}
+		if (corners[0].maxY < corners[1].maxY) /* y grows towards bottom */ {
+			calibrationData.yOffset = corners[0].minY;
+			calibrationData.yScale = corners[0].minY - corners[1].maxY;
+		}
+	else /* y drops towards bottom */ {
+			calibrationData.yOffset = corners[0].maxY;
+			calibrationData.yScale = corners[1].minY - corners[0].maxY;
+		}
+	}
+}
+
+}
+
+int16_t centerBoxX;
+int16_t centerBoxY;
+
+void setup()
+{
 	Serial.begin(115200);
-
-	// Initialise the TFT screen
 	tft.init();
-
-	// Set the rotation to the orientation you wish to use in your project before calibration
-	// (the touch coordinates returned then correspond to that rotation only)
 	tft.setRotation(1);
 
-	// Calibrate the touch screen and retrieve the scaling factors
-	touch_calibrate();
-
-/*
-	// Replace above line with the code sent to Serial Monitor
-	// once calibration is complete, e.g.:
-	uint16_t calData[5] = { 286, 3534, 283, 3600, 6 };
-	tft.setTouch(calData);
-*/
-
-	// Clear the screen
 	tft.fillScreen(TFT_BLACK);
+	// tft.setCursor(20, 20);
+	// tft.setTextSize(1);
+	// tft.setTextColor(TFT_WHITE, TFT_BLACK);
+	// tft.println("Touch corners as indicated");
+	touch::calibrate();
+
+	char buffer[256];
+	touch::calibrationData.snprintf(buffer, sizeof(buffer));
+	Serial.println(buffer);
+
+	tft.fillScreen(TFT_BLACK);
+
+	centerBoxX = tft.width()/3;
+	centerBoxY = tft.height()/3;
 }
 
-int16_t lastTextWidth = 0;
-
-void loop(void) {
+void loop()
+{
 	uint16_t x, y, z;
 
+	char buffer[64];
+	tft.setCursor(centerBoxX, centerBoxY);
+
+	// Raw values
 	tft.getTouchRaw(&x, &y);
 	z = tft.getTouchRawZ();
-	char buffer[32];
-	sprintf(buffer, "x=%5u,y=%5u,z=%5u", x, y, z);
-	// Serial.println(buffer);
-	tft.fillRect(tft.width()/3, tft.height()/2, lastTextWidth, 16, TFT_BLACK);
-	lastTextWidth = tft.drawString(buffer, tft.width()/3, tft.height()/2, 2);
-	
-	// Pressed will be set true is there is a valid touch on the screen
-	bool pressed = tft.getTouch(&x, &y);
+	sprintf(buffer, "rx=%5u ry=%5u rz=%5u     \n", x, y, z);
+	tft.setCursor(centerBoxX, tft.getCursorY());
+	tft.print(buffer);
 
-	// Draw a white spot at the detected coordinates
-	if (pressed) {
+	// Converted values
+	if (touch::getTouch(x, y)) {
 		tft.fillCircle(x, y, 2, TFT_WHITE);
-		// Serial.print("x,y = ");
-		// Serial.print(x);
-		// Serial.print(",");
-		// Serial.println(y);
+	}
+	sprintf(buffer, " x=%5u  y=%5u     \n", x, y);
+	tft.setCursor(centerBoxX, tft.getCursorY());
+	tft.print(buffer);
+
+	// Converted values (after average)
+	if (touch::getTouchAverage(x, y)) {
+		// tft.fillCircle(x, y, 2, TFT_GREEN);
+	}
+	sprintf(buffer, "ax=%5u ay=%5u     \n", x, y);
+	tft.setCursor(centerBoxX, tft.getCursorY());
+	tft.print(buffer);
+
+	// Clearing canvas by pressing up-left corner of the center box
+	if (centerBoxX < x && x < centerBoxX + 40 && 
+		centerBoxY < y && y < centerBoxY + 40) {
+		tft.fillScreen(TFT_BLACK);
 	}
 }
